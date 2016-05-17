9a43d4067df0350f6459b99ebc879a5d0f3cc8a1
duarte.mati@gmail.com <duarte.mati@gmail.com>
2016-05-16 21:22:45 -0300

    Subo POM y una clase de prueba para la RESTApi

--- Server/src/service/ServiceContainer.java
+++ Server/src/service/ServiceContainer.java
@@ -1,15 +1,16 @@
 package service;
 
 import java.util.HashMap;
+
 /**
- * Cada vez que se cree un servicio nuevo
- * se lo debe agregar a esta clase
+ * Cada vez que se cree un servicio nuevo se lo debe agregar a esta clase
  * @author matias
  *
  */
 public class ServiceContainer {
 	
 	private static HashMap <String, Service> listOfService;
+	
 	/**
 	 * Agregar los nuevos servicios a la lista
 	 * @return listOfService
